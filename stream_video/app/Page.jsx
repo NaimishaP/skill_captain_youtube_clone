0d88c161aee158516f2e1612716e15eb5269aeb9
NaimishaP <151544385+NaimishaP@users.noreply.github.com>
2024-02-12 19:44:20 +0000

    Create Page.jsx

---
+++ stream_video/app/Page.jsx
@@ -0,0 +1,15 @@
+// Page.jsx
+import React from 'react';
+"use client"
+import React from 'react';
+import MediaPlayer from '@/components/MediaPlayer';
+import Suggestion from '@/components/Suggestion';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <Button />
+    </div>
+  );
+}
